01fc63eafa5170947c07fd14fd0be5859d13a0cf
Harishkumaran S <harishkumaran654@gmail.com>
2024-09-16 09:46:40 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,39 @@
-        // Function to make the page go fullscreen
-        function goFullscreen() {
-            const element = document.documentElement; // Get the whole page (html element)
-            
-            if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.mozRequestFullScreen) { // Firefox
-                element.mozRequestFullScreen();
-            } else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
-                element.webkitRequestFullscreen();
-            } else if (element.msRequestFullscreen) { // IE/Edge
-                element.msRequestFullscreen();
-            }
-        }
+   // Function to toggle between fullscreen and normal screen
+   function toggleFullscreen() {
+      if (!document.fullscreenElement &&    // Standard method
+          !document.mozFullScreenElement && // Firefox
+          !document.webkitFullscreenElement && // Chrome, Safari, Opera
+          !document.msFullscreenElement) {  // IE/Edge
+          // Enter fullscreen
+          const element = document.documentElement;
+          if (element.requestFullscreen) {
+              element.requestFullscreen();
+          } else if (element.mozRequestFullScreen) { // Firefox
+              element.mozRequestFullScreen();
+          } else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
+              element.webkitRequestFullscreen();
+          } else if (element.msRequestFullscreen) { // IE/Edge
+              element.msRequestFullscreen();
+          }
+      } else {
+          // Exit fullscreen
+          if (document.exitFullscreen) {
+              document.exitFullscreen();
+          } else if (document.mozCancelFullScreen) { // Firefox
+              document.mozCancelFullScreen();
+          } else if (document.webkitExitFullscreen) { // Chrome, Safari, Opera
+              document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { // IE/Edge
+              document.msExitFullscreen();
+          }
+      }
+  }
 
-        // Try to automatically go fullscreen when the page loads
-        window.onload = function() {
-            goFullscreen();
-        }
-
+  // Add a double-click event listener to the entire page
+  document.addEventListener("dblclick", toggleFullscreen);
+  window.onload = function() {
+      toggleFullscreen();
+  }
 // SHOW MENU
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
