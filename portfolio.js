91a417deaf1696059cb86244cc235e29e6c00d1e
Harishkumaran S <harishkumaran654@gmail.com>
2024-09-16 09:08:52 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,24 @@
+        // Function to make the page go fullscreen
+        function goFullscreen() {
+            const element = document.documentElement; // Get the whole page (html element)
+            
+            if (element.requestFullscreen) {
+                element.requestFullscreen();
+            } else if (element.mozRequestFullScreen) { // Firefox
+                element.mozRequestFullScreen();
+            } else if (element.webkitRequestFullscreen) { // Chrome, Safari, Opera
+                element.webkitRequestFullscreen();
+            } else if (element.msRequestFullscreen) { // IE/Edge
+                element.msRequestFullscreen();
+            }
+        }
+
+        // Try to automatically go fullscreen when the page loads
+        window.onload = function() {
+            goFullscreen();
+        }
 
 // SHOW MENU
-
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
           nav = document.getElementById(navId)
