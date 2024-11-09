16ef608d386cc510e34ce622d45a53f35d8ff8e7
Harish <harishkumaran654@gmail.com>
2024-11-09 16:21:47 +0530

    animated  button and align code

--- assets/js/script.js
+++ assets/js/script.js
@@ -235,9 +235,9 @@ const form = document.forms['contact-form']
 
 
 form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
-  .then(() => { window.location.reload(); })
-  .catch(error => console.error('Error!', error.message))
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => alert("Thank you! your form is submitted successfully."))
+        .then(() => { window.location.reload(); })
+        .catch(error => console.error('Error!', error.message))
 })
